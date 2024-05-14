aec71d3cc9b32d1012ac115848e38e8fb6f1ec79
Rock3tRaccoon <jfath2004@outlook.com>
2024-05-14 05:45:07 -0400

    Working Release

--- iFindMem-App/DetailViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface DetailViewController : UITableViewController
-
-@property (strong, nonatomic) NSDate *detailItem;
-@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
-
-@end
-
--- iFindMem-App/RevRootViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@class DetailViewController;
-
-@interface RevRootViewController : UITableViewController
-
-@property (strong, nonatomic) DetailViewController *detailViewController;
-
-@end
