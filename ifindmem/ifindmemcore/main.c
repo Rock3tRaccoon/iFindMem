e8e6e72029d32cbae45d0f78637fbcd031831a64
Rock3tRaccoon <jfath2004@outlook.com>
2024-05-17 07:50:58 -0400

    Complete Rewrite

--- ifindmem/ifindmemcore/main.c
+++ ifindmem/ifindmemcore/main.c
@@ -1,5 +1,66 @@
 #include "mem.h"
 
+void report_error(const char *message) {
+    fprintf(stderr, ERROR"%s\n", message);
+}
+
+// Define an enum for the commands
+enum Command {
+    CMD_HELP,
+    CMD_EXIT,
+    CMD_PID,
+    CMD_READLINES,
+    CMD_READLINESCHAR,
+    CMD_SEARCH,
+    CMD_SEARCHONE,
+    CMD_SEARCHSTR,
+    CMD_SEARCHSTRONE,
+    CMD_SEARCHRANGE,
+    CMD_WRITE,
+    CMD_WRITESTR,
+    CMD_PAUSE,
+    CMD_RESUME,
+    CMD_RESULTS,
+    CMD_UNKNOWN
+};
+
+// Function to get the command based on the first argument
+enum Command getCommand(char *firstArg) {
+    if (strcmp(firstArg, "help\n") == 0) {
+        return CMD_HELP;
+    } else if (strcmp(firstArg, "exit\n") == 0 || strcmp(firstArg, "e\n") == 0) {
+        return CMD_EXIT;
+    } else if (strcmp(firstArg, "pid\n") == 0) {
+        return CMD_PID;
+    } else if (strcmp(firstArg, "readlines") == 0 || strcmp(firstArg, "rl") == 0) {
+        return CMD_READLINES;
+    } else if (strcmp(firstArg, "readlineschar") == 0 || strcmp(firstArg, "rlc") == 0) {
+        return CMD_READLINESCHAR;
+    } else if (strcmp(firstArg, "search") == 0 || strcmp(firstArg, "s") == 0) {
+        return CMD_SEARCH;
+    } else if (strcmp(firstArg, "searchone") == 0 || strcmp(firstArg, "so") == 0) {
+        return CMD_SEARCHONE;
+    } else if (strcmp(firstArg, "searchstr") == 0 || strcmp(firstArg, "ss") == 0) {
+        return CMD_SEARCHSTR;
+    } else if (strcmp(firstArg, "searchstrone") == 0 || strcmp(firstArg, "sso") == 0) {
+        return CMD_SEARCHSTRONE;
+    } else if (strcmp(firstArg, "searchrange") == 0 || strcmp(firstArg, "sr") == 0) {
+        return CMD_SEARCHRANGE;
+    } else if (strcmp(firstArg, "write") == 0 || strcmp(firstArg, "w") == 0) {
+        return CMD_WRITE;
+    } else if (strcmp(firstArg, "writestr") == 0 || strcmp(firstArg, "ws") == 0) {
+        return CMD_WRITESTR;
+    } else if (strcmp(firstArg, "pause\n") == 0 || strcmp(firstArg, "p\n") == 0) {
+        return CMD_PAUSE;
+    } else if (strcmp(firstArg, "resume\n") == 0 || strcmp(firstArg, "r\n") == 0) {
+        return CMD_RESUME;
+    } else if (strcmp(firstArg, "results\n") == 0 || strcmp(firstArg, "res\n") == 0) {
+        return CMD_RESULTS;
+    } else {
+        return CMD_UNKNOWN;
+    }
+}
+
 void interact(pid_t pid, mach_port_t task) {
     char input[128];
     char args[10][30];
@@ -13,7 +74,6 @@ void interact(pid_t pid, mach_port_t task) {
     printf(GOOD"To list iFindMemory commands, type 'help'\n");
 
     while (1) {
-
         memset(args, 0, sizeof(args[0][0])*10*30);
 
         printf(CYAN ">> " WHITE);
@@ -27,188 +87,145 @@ void interact(pid_t pid, mach_port_t task) {
         input[strlen(input)] = ' ';
 
         for (int i = 0; i < strlen(input); i++) {
-
-            if (input[i] == '"') isInQuote = !isInQuote;
-            else if (input[i] == ' ' && !isInQuote) {
+            if (input[i] == '"') isInQuote =!isInQuote;
+            else if (input[i] == ' ' &&!isInQuote) {
                 argCharIndex = 0;
                 for (; charIndex < i; charIndex++) {
                     if (input[charIndex] == '"') continue;
                     args[argIndex][argCharIndex] = input[charIndex];
                     argCharIndex++;
                 }
-                //printf("Argument #%d = %s\n", argIndex, args[argIndex]); //DEBUG
                 argIndex++;
                 charIndex++;
             }
         }
+        // Get the command enum value
+        enum Command cmd = getCommand(args[0]);
 
-        //HELP
-        if (strcmp(args[0], "help\n") == 0) {
-            printf(GOOD"List of commands:\n"
-                   MAGENTA "search (s) " GREEN "[bytes] " WHITE "- search " GREEN"[bytes] " WHITE "(ex: s 434231)\n"
-                   MAGENTA "searchstr (ss) " GREEN "[string] " WHITE "- search " GREEN"[string] " WHITE "(ex: ss CBA)\n"
-                   MAGENTA "searchone (so) " GREEN "[bytes] " WHITE "- search " GREEN"[bytes] " WHITE "for one result(ex: s 434231)\n"
-                   MAGENTA "searchstrone (sso) " GREEN "[string] " WHITE "- search " GREEN"[string] " WHITE "for one result (ex: ss CBA)\n"
-                   MAGENTA "searchrange (sr) " GREEN "[string] [string]" WHITE " - search from " GREEN " [string]" WHITE " to another " GREEN "[string] " WHITE "(ex: sr CBA ABC)\n" 
-                   MAGENTA "write (w) " GREEN "[bytes] " WHITE "- write " GREEN"[bytes] " WHITE "to scanned address (ex: w 434231)\n"
-                   MAGENTA "writestr (ws) " GREEN "[string] " WHITE "- write " GREEN"[string] " WHITE "to scanned address (ex: ws CBA)\n"
-                   MAGENTA "readlines (rl) " GREEN "0x[address] [lines] " WHITE "- read " GREEN "[lines] " WHITE "of " GREEN "[address]"WHITE"\n"
-                   MAGENTA "readlineschar (rlc) " GREEN "0x[address] [lines]" WHITE "- read " GREEN "[lines] " WHITE "of " GREEN "[address] "WHITE"as chars\n"
-                   MAGENTA "pid " GREEN "[pid] " WHITE "- changes pid to new [pid]\n"
-                   MAGENTA "pause (p) " WHITE "- pauses task\n"
-                   MAGENTA "resume (re) " WHITE "- resumes task\n"
-                   MAGENTA "results (res) " WHITE "- prints search results\n"
-                   MAGENTA "quit (q) " WHITE "- exits iFindMemory\n");
+        // Use the switch statement to handle commands
+        switch (cmd) {
+
+
+
+        case CMD_HELP:
+                printf(GOOD"List of commands:\n"
+           MAGENTA "search (s) " GREEN "[bytes] " WHITE "- search " GREEN"[bytes] " WHITE "(ex: s 434231)\n"
+           MAGENTA "searchstr (ss) " GREEN "[string] " WHITE "- search " GREEN"[string] " WHITE "(ex: ss CBA)\n"
+           MAGENTA "searchone (so) " GREEN "[bytes] " WHITE "- search " GREEN"[bytes] " WHITE "for one result(ex: s 434231)\n"
+           MAGENTA "searchstrone (sso) " GREEN "[string] " WHITE "- search " GREEN"[string] " WHITE "for one result (ex: ss CBA)\n"
+           MAGENTA "searchrange (sr) " GREEN "[string] [string]" WHITE " - search from " GREEN " [string]" WHITE " to another " GREEN "[string] " WHITE "(ex: sr CBA ABC)\n" 
+           MAGENTA "write (w) " GREEN "[bytes] " WHITE "- write " GREEN"[bytes] " WHITE "to scanned address (ex: w 434231)\n"
+           MAGENTA "writestr (ws) " GREEN "[string] " WHITE "- write " GREEN"[string] " WHITE "to scanned address (ex: ws CBA)\n"
+           MAGENTA "readlines (rl) " GREEN "0x[address] [lines] " WHITE "- read " GREEN "[lines] " WHITE "of " GREEN "[address]"WHITE"\n"
+           MAGENTA "readlineschar (rlc) " GREEN "0x[address] [lines]" WHITE "- read " GREEN "[lines] " WHITE "of " GREEN "[address] "WHITE"as chars\n"
+           MAGENTA "pid " GREEN "[pid] " WHITE "- changes pid to new [pid]\n"
+           MAGENTA "pause (p) " WHITE "- pauses task\n"
+           MAGENTA "resume (re) " WHITE "- resumes task\n"
+           MAGENTA "results (res) " WHITE "- prints search results\n"
+           MAGENTA "quit (q) " WHITE "- exits iFindMemory\n");
+
+            break;
+
+
+        case CMD_EXIT:
+            printf(GOOD"Exiting iFindMemory....\n");
+            exit(0);
+            break;
+                
+        case CMD_PID:
+            if (args[1][0]!= '\0') {
+                pid = (int) strtol(args[1], NULL, 0);
+                kern_return_t changetfp = task_for_pid(mach_task_self(), pid, &task);
+                if (changetfp == KERN_SUCCESS) {
+                    printf(GOOD"Changed pid to %d\n", pid);
+                } else {
+                    report_error("Could not change PID\n");
+                }
+            } else {
+                report_error("Not enough arguments for pid!\n");
             }
-
-
-
-        //EXIT
-        else if (strcmp(args[0], "exit\n") == 0 || strcmp(args[0], "e\n") == 0) {
-            EXIT
-        }
-        else if (strcmp(args[0], "quit\n") == 0 || strcmp(args[0], "q\n") == 0) {
-            EXIT
-        }
-
-        //change pid
-        else if (strcmp(args[0], "pid\n") == 0) {
-            printf(GREEN "%d" WHITE "\n", pid);
-        }
-        else if (strcmp(args[0], "pid") == 0 && args[1][0] != '\0') {
-            pid = (int) strtol(args[1], NULL, 0);
-            kern_return_t changetfp = task_for_pid(mach_task_self(), pid, &task);
-            if (changetfp == KERN_SUCCESS) {
-                printf(GOOD"Changed pid to %d\n", pid);
+            break;
+      
+        case CMD_READLINES:
+            if (args[1][0]!= '\0' && args[2][0]!= '\0') {
+                read_lines(task, (vm_address_t) strtol(args[1], NULL, 0), (int) strtol(args[2], NULL, 0), false);
+            } else {
+                report_error("Not enough arguments for readlines!\n");
             }
-            else {
-                printf(ERROR"Could not change PID\n");
+            break;
+  
+        case CMD_READLINESCHAR:
+            if (args[1][0]!= '\0' && args[2][0]!= '\0') {
+                read_lines(task, (vm_address_t) strtol(args[1], NULL, 0), (int) strtol(args[2], NULL, 0), true);
+            } else {
+                report_error("Not enough arguments for readlineschar!\n");
             }
-        }
+            break;
 
-        //read_lines (3 args)
-        else if ((strcmp(args[0], "readlines") == 0 || strcmp(args[0], "rl") == 0) && args[1][0] != '\0' && args[2][0] != '\0') {
-            read_lines(task, (vm_address_t) strtol(args[1], NULL, 0), (int) strtol(args[2], NULL, 0), false);
-        }
-        else if ((strcmp(args[0], "readlines") == 0 || strcmp(args[0], "rl") == 0) && args[1][0] != '\0' && args[2][0] == '\0') {
-            printf(ERROR"Not enough arguments for readlines!\n");
-        }
-        else if (strcmp(args[0], "readlines\n") == 0 || strcmp(args[0], "rl\n") == 0) {
-            printf(ERROR"Not enough arguments for readlines!\n");
-        }
+        case CMD_SEARCHSTR:
+            if (args[1][0]!= '\0') {
+                get_region_size(task, &base, &end);
+                printf(GOOD"Finding string - %s\n", args[1]);
+                search_data(task, true, false, base, end, (vm_address_t **)&out, &resultnum, args[1]);
+            } else {
+                report_error("Not enough arguments for searchstr!\n");
+            }
+            break;
 
-        //read_lines (char) (3 args)
-        else if ((strcmp(args[0], "readlineschar") == 0 || strcmp(args[0], "rlc") == 0) && args[1][0] != '\0' && args[2][0] != '\0') {
-            read_lines(task, (vm_address_t) strtol(args[1], NULL, 0), (int) strtol(args[2], NULL, 0), true);
-        }
-        else if ((strcmp(args[0], "readlineschar") == 0 || strcmp(args[0], "rlc") == 0) && args[1][0] != '\0' && args[2][0] == '\0') {
-            printf(ERROR"Not enough arguments for readlineschar!\n");
-        }
-        else if (strcmp(args[0], "readlineschar\n") == 0 || strcmp(args[0], "rlc\n") == 0) {
-            printf(ERROR"Not enough arguments for readlineschar!\n");
-        }
+        case CMD_SEARCHSTRONE:
+            if (args[1][0]!= '\0') {
+                get_region_size(task, &base, &end);
+                printf(GOOD"Finding string - %s\n", args[1]);
+                search_data(task, true, true, base, end, (vm_address_t **)&out, &resultnum, args[1]);
+            } else {
+                report_error("Not enough arguments for searchstrone!\n");
+            }
+            break;
 
-        //search_read (2 args)
-        else if ((strcmp(args[0], "search") == 0 || strcmp(args[0], "s") == 0) && args[1][0] != '\0') {
-            get_region_size(task, &base, &end);
-            printf(GOOD"Memory Range = 0x%lx - 0x%lx\n", base, end);
-            printf(GOOD"Finding bytes - %s\n", args[1]);
-            search_data(task, false, false, base, end, (vm_address_t **)&out, &resultnum, args[1]);
-        }
-        else if (strcmp(args[0], "search\n") == 0 || strcmp(args[0], "s\n") == 0) {
-            printf(ERROR"Not enough arguments for search!\n");
-        }
-        //search_read (2 args)
-        else if ((strcmp(args[0], "searchone") == 0 || strcmp(args[0], "so") == 0) && args[1][0] != '\0') {
-            get_region_size(task, &base, &end);
-            printf(GOOD"Memory Range = 0x%lx - 0x%lx\n", base, end);
-            printf(GOOD"Finding bytes - %s\n", args[1]);
-            search_data(task, false, true, base, end, (vm_address_t **)&out, &resultnum, args[1]);
-        }
-        else if (strcmp(args[0], "searchone\n") == 0 || strcmp(args[0], "so\n") == 0) {
-            printf(ERROR"Not enough arguments for search!\n");
-        }
-        //search_read string(2 args)
-        else if ((strcmp(args[0], "searchstr") == 0 || strcmp(args[0], "ss") == 0) && args[1][0] != '\0') {
-            get_region_size(task, &base, &end);
-            printf(GOOD"Memory Range = 0x%lx - 0x%lx\n", base, end);
-            printf(GOOD"Finding string - %s\n", args[1]);
-            search_data(task, true, false, base, end, (vm_address_t **)&out, &resultnum, args[1]);
-        }
-        else if (strcmp(args[0], "searchstr\n") == 0 || strcmp(args[0], "ss\n") == 0) {
-            printf(ERROR"Not enough arguments for searchstr!\n");
-        }
-        //search_read string(2 args)
-        else if ((strcmp(args[0], "searchstrone") == 0 || strcmp(args[0], "sso") == 0) && args[1][0] != '\0') {
-            get_region_size(task, &base, &end);
-            printf(GOOD"Memory Range = 0x%lx - 0x%lx\n", base, end);
-            printf(GOOD"Finding string - %s\n", args[1]);
-            search_data(task, true, true, base, end, (vm_address_t **)&out, &resultnum, args[1]);
-        }
-        else if (strcmp(args[0], "searchstrone\n") == 0 || strcmp(args[0], "sso\n") == 0) {
-            printf(ERROR"Not enough arguments for searchstr!\n");
-        }
-        else if ((strcmp(args[0], "searchrange") == 0 || strcmp(args[0], "sr") == 0) && args[1][0] != '\0' && args[2][0] != '\0') {
-            get_region_size(task, &base, &end);
-            printf(GOOD"Memory Range = 0x%lx - 0x%lx\n", base, end);
-            printf(GOOD"Finding string - %s...%s\n", args[1], args[2]);
-            search_with_range(task, true, base, end, (vm_address_t **)&out, &resultnum, args[1], args[2]);
-        }
-        else if (strcmp(args[0], "searchrange\n") == 0 || strcmp(args[0], "sr\n") == 0) {
-            printf(ERROR"Not enough arguments for searchrange!\n");
-        }
+        case CMD_SEARCHRANGE:
+            if (args[1][0]!= '\0' && args[2][0]!= '\0') {
+                get_region_size(task, &base, &end);
+                printf(GOOD"Finding string range - %s to %s\n", args[1], args[2]);
+                search_with_range(task, true, base, end, (vm_address_t **)&out, &resultnum, args[1], args[2]);
+            } else {
+                report_error("Not enough arguments for searchrange!\n");
+            }
+            break;
 
+        case CMD_WRITE:
+            if (args[1][0]!= '\0') {
+                write_data(task, false, out, args[1]);
+            } else {
+                report_error("Not enough arguments for write!\n");
+            }
+            break;
 
-        //write_data (2 args)
-        else if ((strcmp(args[0], "write") == 0 || strcmp(args[0], "w") == 0) && args[1][0] != '\0') {
-            write_data(task, false, out, args[1]);
-        }
-        else if (strcmp(args[0], "write\n") == 0 || strcmp(args[0], "w\n") == 0) {
-            printf(ERROR"Not enough arguments for write!\n");
-        }
-        //write_data string (2 args)
-        else if ((strcmp(args[0], "writestr") == 0 || strcmp(args[0], "ws") == 0) && args[1][0] != '\0') {
-            write_data(task, true, out, args[1]);
-        }
-        else if (strcmp(args[0], "writestr\n") == 0 || strcmp(args[0], "ws\n") == 0) {
-            printf(ERROR"Not enough arguments for writestr!\n");
-        }
+        case CMD_WRITESTR:
+            if (args[1][0]!= '\0') {
+                write_data(task, true, out, args[1]);
+            } else {
+                report_error("Not enough arguments for writestr!\n");
+            }
+            break;
 
-
-
-        //pause / resume
-        else if (strcmp(args[0], "pause\n") == 0 || strcmp(args[0], "p\n") == 0) {
-            printf(GOOD"Suspending task...\n");
-            task_suspend(task);
-        }
-        else if (strcmp(args[0], "resume\n") == 0 || strcmp(args[0], "r\n") == 0) {
-            printf(GOOD"Resuming task...\n");
-            task_resume(task);
-        }
-
-        else if (strcmp(args[0], "results\n") == 0 || strcmp(args[0], "res\n") == 0) {
+        case CMD_RESULTS:
             if (resultnum == 0) {
-                printf(ERROR"No results available to print!\n");
-            }
-            else {
+                report_error("No results available to print!");
+            } else {
                 printf(GOOD"Printing %d result(s) from last scan...\n", resultnum);
                 for (int i=0; i<resultnum; i++) {
                     printf("0x%lx\n", out[i]);
                 }
             }
-        }
+            break;
 
-        //null command
-        else if (strcmp(args[0], "\n") == 0) {
-            ;
-        }
-
-        //UNKNOWN COMMAND
-        else {
-            printf(GOOD"Invalid Command\n");
+        default:
+            printf(ERROR"Unknown command\n");
+            break;
         }
     }
-}
+} 
+
 
 
 int main(int argc, char *argv[]) {
@@ -217,34 +234,30 @@ int main(int argc, char *argv[]) {
 
     printf(YELLOW" # Welcome to iFindMemory!\n");
 
-    //check if root
     if (geteuid() && getuid()) {
-        printf(ERROR"Run iFindMemory as root.\n");
+        report_error("Run iFindMemory as root.\n");
         EXIT
     }
 
     if (argc == 1) {
-        //get pid to attach
         printf(GOOD"PID to attach: ");
         scanf("%d", &pid);
         getchar();
-    }
-    else if (argc == 2) {
+    } else if (argc == 2) {
         pid = strtol(argv[1], NULL, 0);
-    }
-    else if (argc > 2) {
-        printf(ERROR"Too many CLI arguments!\n");
+    } else if (argc > 2) {
+        report_error("Too many CLI arguments!\n");
         EXIT
     }
-    //task_for_pid
+
     kern_return_t kret = task_for_pid(mach_task_self(), pid, &task);
-    if (kret != KERN_SUCCESS) {
+    if (kret!= KERN_SUCCESS) {
         printf(ERROR"Couldn't obtain task_for_pid(%d)\n", pid);
-        printf(ERROR"Do you have proper entitlements?\n");
+        report_error("Do you have proper entitlements?\n");
         EXIT
+    } else {
+         printf(GOOD"Obtained task_for_pid(%d)\n", pid);
     }
-    else {
-        printf(GOOD"Obtained task_for_pid(%d)\n", pid); }
 
     interact(pid, task);
     return 0;
